0b788cbbc4be3541d140ace0e5e606417cc84d9d
MarcChesebro <Marc.chesebro@gmail.com>
2020-08-25 15:34:42 -0400

    added basic parse_dice_string wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+use destiny;
 
 /// wrapper
 #[pymodule]
@@ -8,6 +9,11 @@ fn pydestiny(py: Python, m: &PyModule) -> PyResult<()> {
     fn hello_world(_py: Python) -> PyResult<String> {
         let out = "Hello_world";
         Ok(out.to_string())
+    }
+
+    #[pyfn(m, "parse_dice_string")]
+    fn parse_dice_string(_py: Python, string: &str) -> PyResult<i64> {
+        Ok(destiny::parse_dice_string(string))
     }
 
     Ok(())
